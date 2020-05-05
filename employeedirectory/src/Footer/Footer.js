8a57887f67a166bd55fe8910a7d99437dbc89df4
Lori Ross <laross70@gmail.com>
2020-05-04 22:36:45 -0500

    initialize app and added header and footer

---
+++ employeedirectory/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+    return (
+        <div>
+            <h1 className="footer">&#169; Lori Ross</h1>
+        </div>
+    )
+}
+
+export default Footer
